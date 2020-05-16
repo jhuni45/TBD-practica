e22353ffeb7c440a99777e583f503e94c81bd302
Eduardo <angelsoulsword@gmail.com>
2020-05-16 17:57:59 -0500

    times
    arreglo en tiempos

--- laboratorio_2/laboratorio2.c
+++ laboratorio_2/laboratorio2.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
+#include <math.h>
 
-#define M 10
-#define N 10
+
+#define M 1024
+#define N 512
 
 int rows, columns, a[M][N], s[M];
 
@@ -20,27 +23,25 @@ int main() {
     int i, j, *p, rc;
     int sum = 0;
     pthread_t th[M];
+    clock_t firstT, secondT;
+    int secuential[M];
 
-    printf("no. de filas = ");
-    scanf("%d", &rows);
-    printf("no. of columnas = ");
-    scanf("%d", &columns);
-
+    rows = 1024;
+    columns = 512;
     for (i = 0; i < rows; i++) {
         for (j = 0; j < columns; j++) {
-            printf("a[%d][%d] = \n", i, j);
-            scanf("%d", &a[i][j]);
+            a[i][j] = rand() % 5;
         }
     }
 
-    printf("\nLa matriz es: \n");
+    /*printf("\nLa matriz es: \n");
     for(i=0; i < rows; i++) {
         for(j=0; j < columns; j++)
             printf("%d ", a[i][j]);
     printf("\n");
-    }
-
-    
+    }*/
+    //threads
+    firstT=clock();
     for (i=0; i < rows; i++) {
         p = malloc(sizeof(int));
         *p = i;
@@ -50,14 +51,31 @@ int main() {
             exit(-1);
         }
     }
-
+    
     for (i=0; i < rows; i++) {
         pthread_join(th[i], NULL);
     }
-
-    for (i=0; i < rows; i++) {
-        printf("Los valores resultantes son %d\n",s[i]);
+    firstT=clock()-firstT;
+    
+    secondT=clock();
+    //secuencial
+    for (i = 0; i < N; i++)
+    {
+        for ( j = 0; j < M; j++)
+        {
+            secuential[j] = secuential[i]+a[j][i];
+        }
+        
     }
+    secondT=clock()-secondT;
+    
+    printf("El tiempo para la compilacion secuencial es :\n");
+    printf("- %ld\n",secondT);
+    printf("El tiempo para la compilacion con threads es :\n");
+    printf("- %ld\n",firstT);
+    /*for (i=0; i < rows; i++) {
+        printf("Los valores resultantes son %d\n",s[i]);
+    }*/
 
     return 0;
     }
