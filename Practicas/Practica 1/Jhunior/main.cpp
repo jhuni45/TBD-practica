9afee4082a6b224aeaa344dd81794715ec39d308
jhunior <jhunier12@gmail.com>
2020-05-21 23:54:22 -0500

    last

--- Practicas/Practica 1/Jhunior/main.cpp
+++ Practicas/Practica 1/Jhunior/main.cpp
@@ -65,6 +65,23 @@ int add_images(char* file1, char *file2){
         waitKey(10);        
     }
 }
+int gamma(char * file){
+    Mat image1, img32F,aux;
+    image1= imread(file,1);
+    
+    if ( !image1.data ){
+        printf("No image data \n");
+        return -1;
+    }
+    image1.convertTo(img32F, CV_32F);
+    pow(img32F, 0.5, aux);
+    Mat salida;
+    aux.convertTo(salida, CV_8U);
+
+    imshow("orginal ", image1);
+    imshow("gamma", salida);
+    waitKey(0);
+}
 
 int ecualization(char * file){
     Mat img= imread(file, 1);
@@ -96,8 +113,8 @@ int main(int argc, char** argv ){
     }
     //uni_histogram(argv[1]);
     //add_images(argv[1],argv[2]);
-    //gamma(argv[1]);
+    gamma(argv[1]);
     //ecualization(argv[1]);
-    bi_histogram(argv[1]);
+    //bi_histogram(argv[1]);
     return 0;
 }
