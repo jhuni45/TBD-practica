37515ba1e4d779b2414c793ae42d166c73c32369
jhunior <jhunier12@gmail.com>
2020-05-21 23:02:50 -0500

    add testing

--- Practicas/Practica 1/Jhunior/main.cpp
+++ Practicas/Practica 1/Jhunior/main.cpp
@@ -5,14 +5,22 @@
 using namespace cv;
 
 int gray_scale(char * file){
-    Mat image;
+    Mat image,gris,hist;
     image= imread(file,1);
     if ( !image.data ){
         printf("No image data \n");
         return -1;
     }
-    Mat gris;
     cvtColor(image,gris,COLOR_BGR2GRAY);
+    
+    int canales[1]= {0};
+    int bins[1]= {256};
+    float rango[2]= {0, 256};
+    const float *rangos[]= {rango};
+    calcHist(&gris, 1, canales, noArray(), hist, 1, bins, rangos);
+    for (int i= 0; i<256; i++)
+        printf("Celda %d: %g", i, hist.at<float>(i));
+
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
     imshow("Gray Image",gris);
@@ -32,15 +40,33 @@ int add_images(char* file1, char *file2){
         return -1;
     }
     //namedWindow("Display Image", WINDOW_AUTOSIZE );
-    for (int i= 0; i<100; i++) {
+    for (int i= 0; i<10000000; i++) {
         addWeighted(image1, i/100.0, image2, 1-i/100.0, 0, added);
         imshow("Transicion", added);
         waitKey(10);        
     }
 }
 
-int gamma(char * file){
-    
+int ecualization(char * file){
+    Mat img= imread(file, 1);
+    imshow("Entrada", img);
+    Mat gris, hist;
+    cvtColor(img, gris, COLOR_BGR2GRAY);
+    int canales[1]= {0}, bins[1]= {256};
+    float rango[2]= {0, 256};
+    const float *rangos[]= {rango};
+    calcHist(&gris, 1, canales, noArray(), hist, 1, bins, rangos);
+    hist*= 255.0/norm(hist, NORM_L1);
+    Mat lut(1, 256, CV_8UC1);
+    float acum= 0.0;
+    for (int i= 0; i<256; i++) {
+        lut.at<uchar>(0, i)= acum;
+        acum+= hist.at<float>(i);
+    }
+    Mat res;
+    LUT(img, lut, res);
+    imshow("Ecualizada", res);
+    waitKey(0);
 }
 
 int main(int argc, char** argv ){
@@ -50,9 +76,9 @@ int main(int argc, char** argv ){
         return -1;
     }
     //gray_scale(argv[1]);
-    add_images(argv[1],argv[2]);
+    //add_images(argv[1],argv[2]);
     //gamma(argv[1]);
-
+    ecualization(argv[1]);
     
     return 0;
 }
