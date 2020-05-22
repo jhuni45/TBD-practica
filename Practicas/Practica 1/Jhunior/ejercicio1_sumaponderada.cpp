3d496afe22e3ebf40f66aef6b46ea6cdaf028fd7
jhunior <jhunier12@gmail.com>
2020-05-21 23:33:04 -0500

    ultimos arreglos

--- Practicas/Practica 1/Jhunior/ejercicio1_sumaponderada.cpp
+++ Practicas/Practica 1/Jhunior/ejercicio1_sumaponderada.cpp
@@ -4,28 +4,7 @@
 
 using namespace cv;
 
-int gray_scale(char * file){
-    Mat image,gris,hist;
-    image= imread(file,1);
-    if ( !image.data ){
-        printf("No image data \n");
-        return -1;
-    }
-    cvtColor(image,gris,COLOR_BGR2GRAY);
-    
-    int canales[1]= {0};
-    int bins[1]= {256};
-    float rango[2]= {0, 256};
-    const float *rangos[]= {rango};
-    calcHist(&gris, 1, canales, noArray(), hist, 1, bins, rangos);
-    for (int i= 0; i<256; i++)
-        printf("Celda %d: %g", i, hist.at<float>(i));
 
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    imshow("Gray Image",gris);
-    waitKey(0);
-}
 
 int add_images(char* file1, char *file2){
     Mat image1, image2, added;
@@ -47,38 +26,13 @@ int add_images(char* file1, char *file2){
     }
 }
 
-int ecualization(char * file){
-    Mat img= imread(file, 1);
-    imshow("Entrada", img);
-    Mat gris, hist;
-    cvtColor(img, gris, COLOR_BGR2GRAY);
-    int canales[1]= {0}, bins[1]= {256};
-    float rango[2]= {0, 256};
-    const float *rangos[]= {rango};
-    calcHist(&gris, 1, canales, noArray(), hist, 1, bins, rangos);
-    hist*= 255.0/norm(hist, NORM_L1);
-    Mat lut(1, 256, CV_8UC1);
-    float acum= 0.0;
-    for (int i= 0; i<256; i++) {
-        lut.at<uchar>(0, i)= acum;
-        acum+= hist.at<float>(i);
-    }
-    Mat res;
-    LUT(img, lut, res);
-    imshow("Ecualizada", res);
-    waitKey(0);
-}
-
 int main(int argc, char** argv ){
     double alpha_add=0.5;
     if ( argc != 3 ){
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    //gray_scale(argv[1]);
-    //add_images(argv[1],argv[2]);
-    //gamma(argv[1]);
-    ecualization(argv[1]);
+    add_images(argv[1],argv[2]);
     
     return 0;
 }
